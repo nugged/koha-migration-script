8538b86e01df7fd25ccc4b5b3e90dc0eb0a25f47
Andrew Nugged <nugged@gmail.com>
2020-08-04 15:40:09 +0300

    Tidyfy source code: just white space changes (KohaCommunity/xt/perltidyrc used)

--- pre-kc-upgrade-modifications.pl
+++ pre-kc-upgrade-modifications.pl
@@ -23,129 +23,126 @@ use C4::Context;
 my $dbh = C4::Context->dbh;
 
 sub deleteUnneededData {
-  my @tables_to_truncate = (
-			  "atomicupdates",
-#			  "action_logs_cache", # needs to be preserved for statistics
-			  "api_keys",
-			 );
-  my @tables_to_delete = (
-			  "borrower_permissions",
-			  "permission_modules",
-			 );
+    my @tables_to_truncate = (
+        "atomicupdates",
 
-  foreach my $table (@tables_to_truncate) {
-    $dbh->do("DROP TABLE IF EXISTS " . $table);
-  }
+        # "action_logs_cache", # needs to be preserved for statistics
+        "api_keys",
+    );
+    my @tables_to_delete = ( "borrower_permissions", "permission_modules", );
 
-  foreach my $table (@tables_to_delete) {
-    $dbh->do("DELETE IGNORE FROM " . $table);
-  }
+    foreach my $table (@tables_to_truncate) {
+        $dbh->do( "DROP TABLE IF EXISTS " . $table );
+    }
+
+    foreach my $table (@tables_to_delete) {
+        $dbh->do( "DELETE IGNORE FROM " . $table );
+    }
 
 }
 
 sub dropUnusedTables {
-  # TODO: Verify tables are dropped
-  my @tables_to_drop = (
-			"vetuma_transaction_accountlines_link",
-			"vetuma_transaction",
-			"payments_transactions_accountlines",
-			"payments_transactions",
-			"collections_tracking",
-			"batch_overlay_diff_header",
-			"batch_overlay_diff",
-			"batch_overlay_reports",
-			"borrower_ss_blocks",
-			"message_queue_items",
-			"borrower_permissions",
-			"user_permissions",
-			"permissions",
-			"userflags",
-			"permission_modules",
-			"overduebills",
-			"overdue_calendar_weekdays",
-			"overdue_calendar_exceptions",
-			"floating_matrix",
-			"atomicupdates",
-			"collections",
-#			"action_logs_cache", # This is needed for statistics, in the future action_logs can be used.
-			"api_keys", # dropping this requires to regenerate api keys when migration is done, community has slightly different implementation of this
-			"biblio_data_elements",
-		       );
+
+    # TODO: Verify tables are dropped
+    my @tables_to_drop = (
+        "vetuma_transaction_accountlines_link",
+        "vetuma_transaction",
+        "payments_transactions_accountlines",
+        "payments_transactions",
+        "collections_tracking",
+        "batch_overlay_diff_header",
+        "batch_overlay_diff",
+        "batch_overlay_reports",
+        "borrower_ss_blocks",
+        "message_queue_items",
+        "borrower_permissions",
+        "user_permissions",
+        "permissions",
+        "userflags",
+        "permission_modules",
+        "overduebills",
+        "overdue_calendar_weekdays",
+        "overdue_calendar_exceptions",
+        "floating_matrix",
+        "atomicupdates",
+        "collections",
+
+        # "action_logs_cache", # This is needed for statistics, in the future action_logs can be used.
+        "api_keys",    # dropping this requires to regenerate api keys when migration is done, community has slightly different implementation of this
+        "biblio_data_elements",
+    );
 
     foreach my $table (@tables_to_drop) {
-	$dbh->do("DROP TABLE IF EXISTS " . $table);
+        $dbh->do( "DROP TABLE IF EXISTS " . $table );
     }
 
 }
 
 sub dropUnusedColumns {
+
     # TODO: Verify column is dropped
     $dbh->do("ALTER TABLE message_queue DROP COLUMN delivery_note");
 }
 
 # Re-creates tables Koha-Suomi fork dropped from KC schema
 sub recreateDeletedTables {
+
     # collections_tracking
-    $dbh->do("
-CREATE TABLE collections_tracking (
-  collections_tracking_id integer(11) NOT NULL auto_increment,
-  colId integer(11) NOT NULL DEFAULT 0 comment 'collections.colId',
-  itemnumber integer(11) NOT NULL DEFAULT 0 comment 'items.itemnumber',
-  PRIMARY KEY (collections_tracking_id)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-	     ");
+    $dbh->do( "
+        CREATE TABLE collections_tracking (
+          collections_tracking_id integer(11) NOT NULL auto_increment,
+          colId integer(11) NOT NULL DEFAULT 0 comment 'collections.colId',
+          itemnumber integer(11) NOT NULL DEFAULT 0 comment 'items.itemnumber',
+          PRIMARY KEY (collections_tracking_id)
+        ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+    " );
 
     # collections
-    $dbh->do("
-CREATE TABLE collections (
-  colId integer(11) NOT NULL auto_increment,
-  colTitle varchar(100) NOT NULL DEFAULT '',
-  colDesc text NOT NULL,
-  colBranchcode varchar(10) DEFAULT NULL, -- 'branchcode for branch where item should be held.'
-  PRIMARY KEY (colId)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-");
+    $dbh->do( "
+        CREATE TABLE collections (
+          colId integer(11) NOT NULL auto_increment,
+          colTitle varchar(100) NOT NULL DEFAULT '',
+          colDesc text NOT NULL,
+          colBranchcode varchar(10) DEFAULT NULL, -- 'branchcode for branch where item should be held.'
+          PRIMARY KEY (colId)
+        ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+    " );
 
     # userflags
-    $dbh->do("
-CREATE TABLE `userflags` (
-  `bit` int(11) NOT NULL default 0,
-  `flag` varchar(30) default NULL,
-  `flagdesc` varchar(255) default NULL,
-  `defaulton` int(11) default NULL,
-  PRIMARY KEY  (`bit`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-	     ");
+    $dbh->do( "
+        CREATE TABLE `userflags` (
+          `bit` int(11) NOT NULL default 0,
+          `flag` varchar(30) default NULL,
+          `flagdesc` varchar(255) default NULL,
+          `defaulton` int(11) default NULL,
+          PRIMARY KEY  (`bit`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+    " );
 
     # permissions
-    $dbh->do("
-CREATE TABLE `permissions` (
-  `module_bit` int(11) NOT NULL DEFAULT 0,
-  `code` varchar(64) NOT NULL DEFAULT '',
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY  (`module_bit`, `code`),
-  CONSTRAINT `permissions_ibfk_1` FOREIGN KEY (`module_bit`) REFERENCES `userflags` (`bit`)
-    ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-	     ");
-
+    $dbh->do( "
+        CREATE TABLE `permissions` (
+          `module_bit` int(11) NOT NULL DEFAULT 0,
+          `code` varchar(64) NOT NULL DEFAULT '',
+          `description` varchar(255) DEFAULT NULL,
+          PRIMARY KEY  (`module_bit`, `code`),
+          CONSTRAINT `permissions_ibfk_1` FOREIGN KEY (`module_bit`) REFERENCES `userflags` (`bit`)
+            ON DELETE CASCADE ON UPDATE CASCADE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+    " );
 
     # user_permissions
-    $dbh->do("
-
-CREATE TABLE `user_permissions` (
-  `borrowernumber` int(11) NOT NULL DEFAULT 0,
-  `module_bit` int(11) NOT NULL DEFAULT 0,
-  `code` varchar(64) DEFAULT NULL,
-  CONSTRAINT `user_permissions_ibfk_1` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `user_permissions_ibfk_2` FOREIGN KEY (`module_bit`, `code`) REFERENCES `permissions` (`module_bit`, `code`)
-     ON DELETE CASCADE ON UPDATE CASCADE
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-	     ");
-
-
-
+    $dbh->do( "
+        CREATE TABLE `user_permissions` (
+          `borrowernumber` int(11) NOT NULL DEFAULT 0,
+          `module_bit` int(11) NOT NULL DEFAULT 0,
+          `code` varchar(64) DEFAULT NULL,
+          CONSTRAINT `user_permissions_ibfk_1` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`)
+            ON DELETE CASCADE ON UPDATE CASCADE,
+          CONSTRAINT `user_permissions_ibfk_2` FOREIGN KEY (`module_bit`, `code`) REFERENCES `permissions` (`module_bit`, `code`)
+             ON DELETE CASCADE ON UPDATE CASCADE
+         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+    " );
 
 }
 
@@ -160,13 +157,13 @@ sub modifyColumns {
 
     $dbh->do("ALTER TABLE subscription DROP INDEX by_biblionumber");
 
-#    $dbh->do("ALTER TABLE reserves DELETE KEY `old_reserves_pickupexpired`");
+    # $dbh->do("ALTER TABLE reserves DELETE KEY `old_reserves_pickupexpired`");
     $dbh->do("ALTER TABLE reserves DROP COLUMN pickupexpired");
-#    $dbh->do("ALTER TABLE old_reserves DELETE KEY `old_reserves_pickupexpired`");
+
+    # $dbh->do("ALTER TABLE old_reserves DELETE KEY `old_reserves_pickupexpired`");
     $dbh->do("ALTER TABLE old_reserves DROP COLUMN pickupexpired");
 
     $dbh->do("ALTER TABLE overduerules DROP COLUMN fine1, DROP COLUMN fine2, DROP COLUMN fine3");
-
 
     $dbh->do("ALTER TABLE issuingrules DROP INDEX issuingrules_selects");
     $dbh->do("ALTER TABLE issuingrules DROP COLUMN issuingrules_id");
